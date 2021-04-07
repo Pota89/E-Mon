772ee57a58a5f46feae185821f3d0917fb71e3c5
Matteo <marmat.angelini@gmail.com>
2021-04-07 22:26:14 +0200

    First partially working MQTT client

--- app/src/main/java/angelini/domotica/MainActivity.kt
+++ app/src/main/java/angelini/domotica/MainActivity.kt
@@ -2,10 +2,20 @@ package angelini.domotica
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import angelini.domotica.data.NetworkClient
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val networkClient=NetworkClient(applicationContext)
+        networkClient.connect(applicationContext)
+        val result=networkClient.isConnected().toString()
+        val toast = Toast.makeText(applicationContext, result, Toast.LENGTH_LONG)
+        toast.show()
+        //networkClient.disconnect()
     }
 }
--- app/src/main/java/angelini/domotica/data/NetworkClient.kt
+++ app/src/main/java/angelini/domotica/data/NetworkClient.kt
@@ -1,9 +1,11 @@
 package angelini.domotica.data
 
+import android.app.Application
 import android.content.Context
+import android.util.Log
+import android.widget.Toast
 import org.eclipse.paho.android.service.MqttAndroidClient
-import org.eclipse.paho.client.mqttv3.MqttConnectOptions
-import org.eclipse.paho.client.mqttv3.MqttException
+import org.eclipse.paho.client.mqttv3.*
 
 const val MQTT_SERVER_URI       = "tcp://io.adafruit.com:1883"
 const val MQTT_CLIENT_ID        = ""
@@ -11,8 +13,49 @@ const val MQTT_USERNAME         = "ExamToGo"
 const val MQTT_PWD              = "aio_QhVU8878VItl07ohHG13IrtQ2EAc"
 
 class NetworkClient(context: Context) {
-    private var mqttClient = MqttAndroidClient(context, MQTT_SERVER_URI, MQTT_CLIENT_ID)
+    //private var mqttClient = MqttAndroidClient(context, MQTT_SERVER_URI, MQTT_CLIENT_ID)
 
+    private lateinit var mqttClient : MQTTClient
+
+    fun connect(context: Context){
+        // Open MQTT Broker communication
+        mqttClient = MQTTClient(context, MQTT_SERVER_URI, MQTT_CLIENT_ID)
+
+        // Connect and login to MQTT Broker
+        mqttClient.connect( MQTT_USERNAME,
+                MQTT_PWD,
+                object : IMqttActionListener {
+                    override fun onSuccess(asyncActionToken: IMqttToken?) {
+                        Log.d(this.javaClass.name, "Connection success")
+
+                        Toast.makeText(context, "MQTT Connection success", Toast.LENGTH_SHORT).show()
+                    }
+
+                    override fun onFailure(asyncActionToken: IMqttToken?, exception: Throwable?) {
+                        Log.d(this.javaClass.name, "Connection failure: ${exception.toString()}")
+
+                        Toast.makeText(context, "MQTT Connection fails: ${exception.toString()}", Toast.LENGTH_SHORT).show()
+
+                    }
+                },
+                object : MqttCallback {
+                    override fun messageArrived(topic: String?, message: MqttMessage?) {
+                        val msg = "Receive message: ${message.toString()} from topic: $topic"
+                        Log.d(this.javaClass.name, msg)
+
+                        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+                    }
+
+                    override fun connectionLost(cause: Throwable?) {
+                        Log.d(this.javaClass.name, "Connection lost ${cause.toString()}")
+                    }
+
+                    override fun deliveryComplete(token: IMqttDeliveryToken?) {
+                        Log.d(this.javaClass.name, "Delivery complete")
+                    }
+                })
+    }
+/*
     fun connect():Boolean{
         val options = MqttConnectOptions()
         options.userName = MQTT_USERNAME
@@ -26,16 +69,16 @@ class NetworkClient(context: Context) {
         }
         return true
     }
-
+*/
     fun isConnected(): Boolean {
-        return mqttClient.isConnected
+        return mqttClient.isConnected()
     }
-
+/*
     fun disconnect() {
         try {
             mqttClient.disconnect()
         } catch (e: MqttException) {
             e.printStackTrace()
         }
-    }
+    }*/
 }
