aefc9cf91e79d2772edd2aeb1e3034bf8ea6297a
Matteo Angelini <marmat.angelini@gmail.com>
2021-07-03 15:55:38 +0200

    Completed import, to fix because doesn't compile

--- app/src/main/java/angelini/domotica/MainActivity.kt
+++ app/src/main/java/angelini/domotica/MainActivity.kt
@@ -2,7 +2,15 @@ package angelini.domotica
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import angelini.domotica.data.Repository
+import com.google.android.material.navigation.NavigationView
 
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +21,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         //repository=Repository(applicationContext)
         //repository.connect()
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        val navController = findNavController(R.id.fragment_container)
+
+        appBarConfiguration = AppBarConfiguration(setOf(R.id.nav_home), drawerLayout)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 
     override fun onDestroy() {
@@ -20,5 +39,14 @@ class MainActivity : AppCompatActivity() {
         super.onDestroy()
     }
 
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.fragment_container)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
 
 }
