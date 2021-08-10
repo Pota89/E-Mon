1fa5c6e7201ec04bad204744cb06cb0d3a2b7c84
Matteo Angelini <marmat.angelini@gmail.com>
2021-08-10 18:51:14 +0200

    Added code to have different device views inside Room Fragment

--- app/src/main/java/angelini/domotica/ui/room/DeviceAdapter.kt
+++ app/src/main/java/angelini/domotica/ui/room/DeviceAdapter.kt
@@ -4,24 +4,65 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
 import angelini.domotica.data.db.Device
+import angelini.domotica.data.db.DeviceType
 import angelini.domotica.databinding.ListItemDeviceBinding
 
 //take Device rows and adapt them for RecyclerView in RoomFragment
-class DeviceAdapter : ListAdapter<Device, DeviceViewHolder>(DeviceDiffCallback()) {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DeviceViewHolder {
-        return DeviceViewHolder(
-            ListItemDeviceBinding.inflate(
-                LayoutInflater.from(parent.context),
-                parent,
-                false
+class DeviceAdapter : ListAdapter<Device, RecyclerView.ViewHolder>(DeviceDiffCallback()) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+
+        when(DeviceType.values()[viewType])//get DeviceType enum from viewType
+        {
+            DeviceType.TEMPERATURE -> return DeviceViewHolder(
+                ListItemDeviceBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
             )
-        )
+
+            DeviceType.MOVEMENT -> return DeviceViewHolder(
+                ListItemDeviceBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+            )
+
+            DeviceType.LAMP -> return DeviceViewHolder(
+                ListItemDeviceBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+            )
+
+            else -> return DeviceViewHolder(
+                ListItemDeviceBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+            )
+
+        }
     }
 
-    override fun onBindViewHolder(holder: DeviceViewHolder, position: Int) {
+    override fun getItemViewType(position: Int): Int {
+        val item=getItem(position)
+        return item.type.ordinal//return Int equivalent of DeviceType Enum
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val device = getItem(position)
-        holder.bind(device)
+        when(DeviceType.values()[holder.itemViewType]){//get DeviceType enum from viewType
+            DeviceType.TEMPERATURE -> (holder as DeviceViewHolder).bind(device)
+            DeviceType.MOVEMENT -> (holder as DeviceViewHolder).bind(device)
+            DeviceType.LAMP -> (holder as DeviceViewHolder).bind(device)
+            else -> (holder as DeviceViewHolder).bind(device)
+        }
     }
 }
 
