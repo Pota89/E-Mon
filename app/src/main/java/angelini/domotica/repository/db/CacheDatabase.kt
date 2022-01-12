4af4993f674a5ce160d6e59e0c0a4b6a01ab2e15
Matteo Angelini <marmat.angelini@gmail.com>
2022-01-12 10:27:40 +0100

    Documentation for CacheDatabase.kt

--- app/src/main/java/angelini/domotica/repository/db/CacheDatabase.kt
+++ app/src/main/java/angelini/domotica/repository/db/CacheDatabase.kt
@@ -4,6 +4,12 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import angelini.domotica.repository.datatypes.Device
 
+/**
+ * Classe astratta che definisce la struttura del database
+ *
+ * L'implementazione è automatizzata in fase di compilazione ed è a
+ * carico della libreria Room di Android.
+ */
 @Database(entities = [Device::class], version = 1, exportSchema = false)
 abstract class CacheDatabase : RoomDatabase() {
     abstract fun deviceDao(): DeviceDao
