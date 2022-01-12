c63fa192634d0aaf941b6a493745c805c945695e
Matteo Angelini <marmat.angelini@gmail.com>
2022-01-12 17:39:16 +0100

    Added more documentation in NetworkClient.kt

--- app/src/main/java/angelini/domotica/repository/network/NetworkClient.kt
+++ app/src/main/java/angelini/domotica/repository/network/NetworkClient.kt
@@ -2,8 +2,6 @@ package angelini.domotica.repository.network
 
 import android.content.Context
 import android.util.Log
-import angelini.domotica.repository.MQTT_PWD
-import angelini.domotica.repository.MQTT_USERNAME
 import org.eclipse.paho.android.service.MqttAndroidClient
 import org.eclipse.paho.client.mqttv3.*
 
@@ -114,10 +112,16 @@ class NetworkClient(context: Context) {
     }
     //end callbacks section
 
-    fun connect():Boolean{
+    /**
+     * Connetti al server MQTT
+     *
+     * @property username nome utente
+     * @property password password dell'utente
+     */
+    fun connect(username:String,password:String):Boolean{
         val options = MqttConnectOptions()
-        options.userName = MQTT_USERNAME
-        options.password = MQTT_PWD.toCharArray()
+        options.userName = username
+        options.password = password.toCharArray()
         mqttClient.setCallback(clientCallbacks)
 
         try {
@@ -129,10 +133,18 @@ class NetworkClient(context: Context) {
         return true
     }
 
+    /**
+     * Verifica se si è connessi al server MQTT
+     */
     fun isConnected(): Boolean {
         return mqttClient.isConnected
     }
 
+    /**
+     * Sottoscrizione a un feed
+     *
+     * @property topic nome del feed
+     */
     fun subscribe(topic:String) {
         try {
             mqttClient.subscribe(topic, 1, null, subscribeCallbacks)
@@ -141,6 +153,11 @@ class NetworkClient(context: Context) {
         }
     }
 
+    /**
+     * Rimozione della sottoscrizione a un feed
+     *
+     * @property topic nome del feed
+     */
     fun unsubscribe(topic:String) {
         try {
             mqttClient.unsubscribe(topic, null, unsubscribeCallbacks)
@@ -149,6 +166,12 @@ class NetworkClient(context: Context) {
         }
     }
 
+    /**
+     * Pubblica un messaggio sul feed
+     *
+     * @property topic nome del feed
+     * @property smg messaggio pubblicato nel feed
+     */
     fun publish(topic:      String,
                 msg:        String) {
         try {
@@ -162,6 +185,9 @@ class NetworkClient(context: Context) {
         }
     }
 
+    /**
+     * Disconetti dal server MQTT attualmente collegato
+     */
     fun disconnect() {
         try {
             mqttClient.disconnect(null,disconnectCallbacks)
