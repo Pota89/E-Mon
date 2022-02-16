267723f260a630fedd29d63c86a38a5f4560e279
Matteo Angelini <marmat.angelini@gmail.com>
2022-02-17 00:46:09 +0100

    Removed debug code in HomeViewModel

--- app/src/main/java/angelini/domotica/ui/home/HomeViewModel.kt
+++ app/src/main/java/angelini/domotica/ui/home/HomeViewModel.kt
@@ -9,20 +9,6 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
 class HomeViewModel(private val repository: Repository): ViewModel(){
-    init {
-        Log.i("HomeViewModel", "We can run HomeViewModel")
-    }
-/*
-    private val _rooms = flow {
-        val lista=mutableListOf<Room>()
-        val kitchenRoom= Room(RoomType.KITCHEN,1)
-        val loungeRoom= Room(RoomType.LOUNGE,1)
-
-        lista.add(kitchenRoom)
-        lista.add(loungeRoom)
-        emit(lista)
-    }*/
-
     val rooms: Flow<List<Room>>
         get() = repository.roomsList
 }
