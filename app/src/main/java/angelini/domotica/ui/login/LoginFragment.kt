c3198ba24ea63f073e990dc793f0b6e35711364b
Matteo Angelini <marmat.angelini@gmail.com>
2022-02-17 02:50:51 +0100

    Removed unused imports in Fragments

--- app/src/main/java/angelini/domotica/ui/login/LoginFragment.kt
+++ app/src/main/java/angelini/domotica/ui/login/LoginFragment.kt
@@ -4,11 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import angelini.domotica.MainApplication
-import angelini.domotica.R
 import angelini.domotica.databinding.FragmentLoginBinding
 import angelini.domotica.ui.RepositoryViewModelFactory
 
