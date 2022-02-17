40e4df5ed660e03a295eed09e912e05d4c4098fd
Matteo Angelini <marmat.angelini@gmail.com>
2022-02-17 02:04:25 +0100

    Cleaned imports in HomeViewModel.kt

--- app/src/main/java/angelini/domotica/ui/home/HomeViewModel.kt
+++ app/src/main/java/angelini/domotica/ui/home/HomeViewModel.kt
@@ -1,12 +1,9 @@
 package angelini.domotica.ui.home
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import angelini.domotica.repository.Repository
 import angelini.domotica.repository.datatypes.Room
-import angelini.domotica.repository.datatypes.RoomType
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 
 class HomeViewModel(private val repository: Repository): ViewModel(){
     val rooms: Flow<List<Room>>
