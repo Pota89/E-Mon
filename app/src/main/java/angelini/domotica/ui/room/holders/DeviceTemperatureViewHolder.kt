f9aec4a9f7ea31b00ad266b868ad8168f7f7ec3d
Matteo Angelini <marmat.angelini@gmail.com>
2022-02-23 12:04:35 +0100

    Updated Temperature holder

--- app/src/main/java/angelini/domotica/ui/room/holders/DeviceTemperatureViewHolder.kt
+++ app/src/main/java/angelini/domotica/ui/room/holders/DeviceTemperatureViewHolder.kt
@@ -2,27 +2,22 @@ package angelini.domotica.ui.room.holders
 
 import androidx.recyclerview.widget.RecyclerView
 import angelini.domotica.repository.datatypes.Device
-import angelini.domotica.repository.datatypes.DeviceType
 import angelini.domotica.databinding.ListItemDeviceTemperatureBinding
 
 class DeviceTemperatureViewHolder(private val binding: ListItemDeviceTemperatureBinding) : RecyclerView.ViewHolder(binding.root) {
 
     fun bind(item: Device)  {
         val builder = StringBuilder()
-
-        when(item.type)
-        {
-            DeviceType.TEMPERATURE -> builder.append("Temperatura")
-            DeviceType.MOVEMENT -> builder.append("Movimento")
-            DeviceType.LAMP -> builder.append("Lampada")
-            else -> builder.append("Sconosciuto")
-        }
+        builder.append("Temperatura")
 
         if (item.number!=0){
             builder.append(" ")
             builder.append(item.number)
         }
 
-        binding.deviceName.text=builder.toString()
+        builder.append(" : ")
+        builder.append(item.value)
+        builder.append("°C")
+        binding.temperatureSensorName.text=builder.toString()
     }
 }
