995f06dd6d3b95fd0a5f02f6de63499faf8c84a8
Matteo Angelini <marmat.angelini@gmail.com>
2022-03-18 17:08:39 +0100

    Fixed language entry

--- app/src/main/java/angelini/domotica/PreferencesValues.kt
+++ app/src/main/java/angelini/domotica/PreferencesValues.kt
@@ -3,7 +3,7 @@ package angelini.domotica
 const val PREF_TITLE_LANG = "appLanguage"
 const val PREF_TITLE_THEME = "appTheme"
 const val PREF_DB_NAME = "app"
-const val LANGUAGE_DEFAULT = "en"
+const val LANGUAGE_DEFAULT = "it"
 const val THEME_DEFAULT = "day"
 
 const val DEPRECATION = "DEPRECATION"
