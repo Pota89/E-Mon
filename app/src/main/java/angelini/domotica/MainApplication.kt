9b3688adfc2090101ceae830e881e40c5281f037
Matteo Angelini <marmat.angelini@gmail.com>
2022-04-04 17:47:57 +0200

    Commented MainApplication.kt

--- app/src/main/java/angelini/domotica/MainApplication.kt
+++ app/src/main/java/angelini/domotica/MainApplication.kt
@@ -9,13 +9,24 @@ import angelini.domotica.repository.network.NetworkClient
 import kotlinx.coroutines.*
 import org.eclipse.paho.android.service.MqttService
 
-
+/**
+ * Punto d'ingresso personalizzato del programma
+ *
+ * Eredita da Application, è necessario per inizializzare il repository
+ * e le sue dipendenze secondo la filosofia Dependency Injection
+ */
 class MainApplication : Application() {
     private lateinit var database:CacheDatabase
     private lateinit var networkClient:NetworkClient
     private lateinit var repository:Repository
 
-
+    /**
+     * Effettua le inizializzazioni necessarie al repository
+     *
+     * In dettaglio, provvede a far partire un servizio dedicato alla comunicazione MQTT,
+     * inizializzazione del database cache e inizializzazione del repository con gli
+     * output precedenti
+     */
     override fun onCreate() {
         super.onCreate()
 
@@ -34,8 +45,17 @@ class MainApplication : Application() {
         repository=Repository(database,networkClient)
     }
 
+    /**
+     * Permette di richiamare il repository dal Context
+     */
     fun getRepository():Repository{ return repository}
 
+    /**
+     * Lancia una coroutine che provvede a disconnettere il repository
+     *
+     * Internamente al repository viene disconnessa la
+     * comunicazione al server MQTT
+     */
     override fun onTerminate() {
         CoroutineScope(Dispatchers.IO).launch {
             repository.disconnect()
