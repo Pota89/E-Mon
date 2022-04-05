635fe9ff02ce1e01fc38150b97cc4ce7edd30ead
Matteo Angelini <marmat.angelini@gmail.com>
2022-04-05 10:42:24 +0200

    Commented DeviceUnknownViewHolder.kt

--- app/src/main/java/angelini/domotica/ui/room/holders/DeviceUnknownViewHolder.kt
+++ app/src/main/java/angelini/domotica/ui/room/holders/DeviceUnknownViewHolder.kt
@@ -4,7 +4,12 @@ import androidx.recyclerview.widget.RecyclerView
 import angelini.domotica.repository.datatypes.Device
 import angelini.domotica.databinding.ListItemDeviceUnknownBinding
 
-
+/**
+ * ViewHolder generico per Device non conosciuti
+ *
+ * Utilizzato quando da rete MQTT viene trovato un dispositivo non riconosciuto
+ * dal programma, ad esempio un valore non riconosciuto dalla Enum Device
+ */
 class DeviceUnknownViewHolder(@Suppress("UNUSED_PARAMETER") val binding: ListItemDeviceUnknownBinding) : RecyclerView.ViewHolder(binding.root) {
     fun bind(@Suppress("UNUSED_PARAMETER")item: Device)  {
     }
